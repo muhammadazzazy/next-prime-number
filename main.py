a74cceeb5029238c817ca3115c3eae82f2391823
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-03 18:00:13 +0200

    added main guard with some refactoring

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from sympy import isprime
 
-count = 1
-while True:
-    choice = input('Do you want the next prime number? (Y/n) ')
-    if choice.lower() == 'y':
-        while not isprime(count):
+
+def main() -> None:
+    count = 1
+    while True:
+        choice = input('Do you want the next prime number? (Y/n) ')
+        if choice.lower() == 'y':
+            while not isprime(count):
+                count += 1
+            print(f'{count} is the next prime number!')
             count += 1
-        print(f'{count} is the next prime number!')
-        count += 1
-    else:
-        quit()
+        else:
+            quit()
+
+
+if __name__ == '__main__':
+    main()
