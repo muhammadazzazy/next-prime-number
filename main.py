ce269bd4ba0024e9afe73ed44d8f8ee63ba099fb
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-14 23:17:04 +0200

    added exception handling and type annotations

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from sympy import isprime
+from sys import exit
 
 
 def main() -> None:
-    count = 1
+    count: int = 1
+    exit_message: str = 'Exiting program...'
     while True:
-        choice = input('Do you want the next prime number? (Y/n) ')
+        try:
+            choice = input('Do you want the next prime number? (Y/n) ')
+        except KeyboardInterrupt:
+            print(exit_message)
         if choice.lower() == 'y':
             while not isprime(count):
                 count += 1
             print(f'{count} is the next prime number!')
             count += 1
         else:
-            quit()
+            print(exit_message)
+            exit()
 
 
 if __name__ == '__main__':
